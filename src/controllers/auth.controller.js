0705876a7a4b631b961d0c0935db3c78d2e5a9e3
Revanshu Pusadkar <revanshu444@gmail.com>
2026-02-13 20:08:27 +0530

    jwt-auth db-connection nodemailer implemented

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,85 @@
+const userModel = require("../models/user.model")
+const jwt = require("jsonwebtoken")
+const emailService = require("../services/email.service")
+
+/**
+ * 
+ * - user register controller  
+ * - POST /api/auth/register
+ */
+async function userRegister(req, res) {
+    const { email, name, password } = req.body
+
+    const isExist = await userModel.findOne({ email }) 
+
+    if(isExist){
+        return res.status(422).json({
+            message:"User already exist",
+            status:"failed"
+        })
+    }
+
+    const user= await userModel.create({
+        email, password, name
+    })
+
+    const token = jwt.sign({userId:user._id}, process.env.JWT_SECRET, {expiresIn:"3d"})
+
+    res.cookie("token", token)
+
+    res.status(201).json({
+        user:{
+            _id:user._id,
+            email:user.email,
+            name:user.name
+        },
+        token: token
+    })
+
+    await emailService.sendRegistrationEmail(user.email, user.name)
+}
+
+/**
+ * 
+ * - user login controller  
+ * - POST /api/auth/login
+ */
+async function userLogin(req, res) {
+    const { email, password } = req.body
+
+    const user = await userModel.findOne({ email }).select("+password")
+
+    if(!user){
+        return res.status(401).json({
+            message:"Email or password is incorrect",
+            status:"failed"
+        })
+    }
+
+        const isValidPassword = await user.comparePassword(password)
+
+    if(!isValidPassword){
+        return res.status(401).json({
+            message:"Email or password is incorrect",
+            status:"failed"
+        })
+    }
+
+    const token = jwt.sign({userId:user._id}, process.env.JWT_SECRET, {expiresIn:"3d"})
+
+    res.cookie("token", token)
+
+    res.status(200).json({
+        user:{
+            _id:user._id,
+            email:user.email,
+            name:user.name
+        },
+        token: token
+    })
+}
+
+module.exports = {
+    userRegister,
+    userLogin
+}
